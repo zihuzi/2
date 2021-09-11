1d1eed4670bcbbb281aaf21d79b3e552c5693001
QWERTY <69460914+zihuzi@users.noreply.github.com>
2021-09-12 00:50:35 +0800

    Add files via upload

---
+++ 2-6.cpp
@@ -0,0 +1,7 @@
+# include <iostream.h>
+void main()
+{										
+	double a;										//声明浮点型变量
+	a= 3.13564824;									//赋值
+	cout<<a<<endl;									//输出
+}
---
+++ 2-8.cpp
@@ -0,0 +1,10 @@
+#include <iostream.h>
+void main()
+{
+	char ch;
+	ch='a';
+	int i=ch;
+	cout<<ch<<endl;
+	cout<<i<<endl;
+}
+
